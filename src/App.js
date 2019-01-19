01099583e6400006b947c95548e1d9de236607fd
want <wantbycre@gmail.com>
2019-01-20 02:27:46 +0900

    movieql server랑 같이 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-import { ApolloProvide } from "react-apollo";
-import client from "./apollo";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvide client={client}>
-        <div className="App" />
-      </ApolloProvide>
+      <ApolloProvider client={client}>
+        <Router>
+          <main>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:movieId"} component={Detail} />
+          </main>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import GlobalStyle from "./globalStyles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
